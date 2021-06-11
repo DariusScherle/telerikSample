f4028434754ab1fc7eba49698dc91961e5b0352a
Darius Scherle <darius.scherle@itree.ch>
2021-06-11 12:19:41 +0200

    init

---
+++ Sample/Client/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sample.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddHttpClient("Default",
+                client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+            builder.Services.AddTransient(
+                sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Default"));
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddTelerikBlazor();
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ Sample/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Window.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return NotFound();
+        }
+    }
+}
